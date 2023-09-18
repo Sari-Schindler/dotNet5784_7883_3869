09572ce20128eb44c532e4d05402c3f61449f8c5
Sara <mss054333@gmail.com>
2023-09-19 01:30:46 +0300

    main

---
+++ dotNet5782_0995_1050/Stage0/Program3869.cs
@@ -0,0 +1,20 @@
+﻿namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome3869();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome7883();
+
+        private static void Welcome3869()
+        {
+            Console.Write("Enter your name: ");
+            string? name = Console.ReadLine();
+            Console.WriteLine(name + " " + "welcome to my first console application\"");
+        }
+    }
+}
