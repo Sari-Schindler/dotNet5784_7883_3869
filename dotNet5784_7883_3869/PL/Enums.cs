0953f217840f7be793866840d56f8fdd4a78bbba
Sara <mss054333@gmail.com>
2024-01-08 23:13:35 +0200

    till 7-b

--- dotNet5784_7883_3869/PL/Enums.cs
+++ dotNet5784_7883_3869/PL/Enums.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PL
+namespace PL;
+
+internal class EngineerExperienceCollection : IEnumerable
 {
-    internal class Enums
-    {
-    }
+    public static readonly IEnumerable<BO.EngineerExperience> e_enums =
+        (Enum.GetValues(typeof(BO.EngineerExperience)) as IEnumerable<BO.EngineerExperience>)!;
+
+    public IEnumerator GetEnumerator() => e_enums.GetEnumerator();
 }
